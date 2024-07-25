f2cdf0d8c788a8215bf0541257163c6b9a06374e
CreamMeatball <pch5445@gmail.com>
2024-07-25 16:01:09 +0900

    run on localhost 3000

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ var app = express();
 app.listen(3000, function() {
     console.log('Server is running on port 3000');
 })
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.use(express.static('public'));
